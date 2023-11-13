93063d4cb5a87ebf74ccd9972443d6cdecf2d376
Dolan Reynolds <reynoldsdolan@gmail.com>
2023-11-13 17:08:27 -0500

    legend text change

--- src/legend.tsx
+++ src/legend.tsx
@@ -8,21 +8,21 @@ const Legend = () => {
           <div className="flex h-4 w-4 items-center justify-center rounded-full bg-blue-800 md:h-6 md:w-6">
             <div className="h-1.5 w-1.5 rounded-full bg-orange-200 md:h-2 md:w-2"></div>
           </div>
-          <p className="text-[10px] font-semibold md:text-sm">IBJJF Events</p>
+          <p className="text-[10px] font-semibold md:text-sm">IBJJF</p>
         </div>
         <div className="flex items-center justify-center gap-2">
           <div className="flex h-4 w-4 items-center justify-center rounded-full bg-orange-400 md:h-6 md:w-6">
             <div className="h-1.5 w-1.5 rounded-full bg-orange-100 md:h-2 md:w-2"></div>
           </div>
           <p className="text-[10px] font-semibold md:text-sm">
-            Grappling Industries Events
+            Grappling Industries
           </p>
         </div>
         <div className="flex items-center justify-center gap-2">
           <div className="flex h-4 w-4 items-center justify-center rounded-full bg-indigo-950 md:h-6 md:w-6">
             <div className="h-1.5 w-1.5 rounded-full bg-purple-900 md:h-2 md:w-2"></div>
           </div>
-          <p className="text-[10px] font-semibold md:text-sm">AJP Events</p>
+          <p className="text-[10px] font-semibold md:text-sm">AJP</p>
         </div>
       </div>
     </div>
