559b503c4c204a053a2693bea2125f9d5eb33818
Dolan Reynolds <reynoldsdolan@gmail.com>
2023-11-16 20:50:41 -0500

    map rendering bug causing lag fixed, adding clerk

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,21 +1,67 @@
 import { FC } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import { Layout } from "@/app/Layout";
 import { Home, NoMatch, About, List } from "@/pages";
+import {
+  ClerkProvider,
+  RedirectToSignIn,
+  SignIn,
+  SignUp,
+  SignedIn,
+  SignedOut,
+} from "@clerk/clerk-react";
+
+// if (!process.env.REACT_APP_CLERK_PUBLISHABLE_KEY) {
+//   throw new Error("Missing Publishable Key");
+// }
+// const clerkPubKey = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
+
+// function ClerkProviderWithRoutes() {
+//   const navigate = useNavigate();
+
+//   return (
+//     <ClerkProvider publishableKey={clerkPubKey} navigate={(to) => navigate(to)}>
+//       <Routes>
+//         <Route path="/" element={<Layout />}>
+//           <Route
+//             path="/sign-in/*"
+//             element={<SignIn routing="path" path="/sign-in" />}
+//           />
+//           <Route
+//             path="/sign-up/*"
+//             element={<SignUp routing="path" path="/sign-up" />}
+//           />
+//           <Route
+//             path="/MyEvents"
+//             element={
+//               <>
+//                 <SignedIn>
+//                   <MyEvents />
+//                 </SignedIn>
+//                 <SignedOut>
+//                   <RedirectToSignIn />
+//                 </SignedOut>
+//               </>
+//             }
+//           />
+//         </Route>
+//       </Routes>
+//     </ClerkProvider>
+//   );
+// }
 
 const App: FC = () => {
-
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="*" element={<NoMatch />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/list" element={<List />} />
-        </Route>
-      </Routes>
-    </>
+    // <ClerkProvider publishableKey={clerkPubKey}>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="*" element={<NoMatch />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/list" element={<List />} />
+      </Route>
+    </Routes>
+    // </ClerkProvider>
   );
 };
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,24 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve("src"),
-      "@widgets": path.resolve("src/widgets"),
-      "@features": path.resolve("src/features"),
-      "@entities": path.resolve("src/entities"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    define: {
+      "process.env.REACT_APP_CLERK_PUBLISHABLE_KEY": JSON.stringify(
+        env.REACT_APP_CLERK_PUBLISHABLE_KEY,
+      ),
     },
-  },
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve("src"),
+        "@widgets": path.resolve("src/widgets"),
+        "@features": path.resolve("src/features"),
+        "@entities": path.resolve("src/entities"),
+      },
+    },
+  };
 });
