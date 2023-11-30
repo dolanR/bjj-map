2b23aea824ee157edd0fbb064650e1b72209a62a
Dolan Reynolds <reynoldsdolan@gmail.com>
2023-11-30 12:54:01 -0500

    mobile responsive 404

--- src/pages/NoMatch/ui/Page/Page.tsx
+++ src/pages/NoMatch/ui/Page/Page.tsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 
 const NoMatch: FC = () => {
   return (
-    <div className="absolute left-1/2 top-1/2  flex -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center gap-4">
-      <h1 className="bg-gradient-to-l from-primary-content via-secondary to-primary bg-clip-text text-9xl font-bold text-transparent">
+    <div className="absolute left-1/2 top-1/2  flex w-[90%] -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center gap-1 text-center">
+      <h1 className="bg-gradient-to-l from-primary-content via-secondary to-primary bg-clip-text text-8xl font-bold text-transparent">
         404
       </h1>
       <p className="text-3xl font-medium text-neutral">Page not found</p>
-      <p className="mx-3 text-center text-2xl font-medium text-neutral">
+      <p className="text-center text-lg font-medium text-neutral md:w-[60%] md:text-2xl">
         This likely happened if you tried to sign in without signing up, try
         clicking the sign up button in the lower portion of the Clerk sign in
         popup
